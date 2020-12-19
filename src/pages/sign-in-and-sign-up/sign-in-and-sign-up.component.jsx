9574787d4685f5cc295cd541c80a9846adcc33b6
Igor Bond <npro27@yandex.ru>
2020-12-19 16:12:45 +1000

    Added sign in component

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignIn from '../../components/sign-in/sign-in.components'
+
+import './sign-in-and-sign-up.styles.scss'
+
+const SignInAndSignUpPage = () => {
+    return (
+        <div className='sign-in-and-sign-up'>
+            <SignIn></SignIn>
+        </div>
+    )
+}
+
+export default SignInAndSignUpPage
